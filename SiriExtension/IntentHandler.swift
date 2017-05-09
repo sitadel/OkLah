2463e67aa2db52b1d23a4cbd28f95249ca2db99a
Seet <Seet@Seets-MacBook-Air.local>
2017-05-09 08:34:52 +0800

    1. Refine the Siri Intent code to write a log to indicate whether the payee or amount is missing. 2. Deleted unused code in Siri Intent.

--- SiriExtension/IntentHandler.swift
+++ SiriExtension/IntentHandler.swift
@@ -8,28 +8,24 @@
 
 import Intents
 
-// As an example, this class is set up to handle Message intents.
-// You will want to replace this or add other intents as appropriate.
-// The intents you wish to handle must be declared in the extension's Info.plist.
-
 // You can test your example integration by saying things to Siri like:
-// "Send a message using <myApp>"
-// "<myApp> John saying hello"
-// "Search for messages in <myApp>"
+// "Send $20 to Benjamin using <ReckonING>"
+// "<ReckonING> Send $20 to Benjamin"
 
 extension IntentHandler: INSendPaymentIntentHandling {
     
     func handle(sendPayment intent: INSendPaymentIntent, completion: @escaping (INSendPaymentIntentResponse) -> Void) {
         // Check that we have valid values for payee and currencyAmount
-        guard let payee = intent.payee, let amount = intent.currencyAmount else {
-            print("Incomplete Siri Intent command parameters: missing Payee or Amount.")
+        guard let payee = intent.payee else {
+            print("Incomplete Siri Intent command parameters: missing Payee.")
             return completion(INSendPaymentIntentResponse(code: .unspecified, userActivity: nil))
         }
-        // Make your payment!
-        print("Sending \(amount) payment to \(payee)!")
-//        makePayment()
-//        completion(INSendPaymentIntentResponse(code: .success, userActivity: nil))
+        guard let amount = intent.currencyAmount else {
+            print("Incomplete Siri Intent command parameters: missing Amount.")
+            return completion(INSendPaymentIntentResponse(code: .unspecified, userActivity: nil))
+        }
         
+        // Make your payment!
         if let url = Server.createTransaction(me:"iamsam", fromBankId: "at02-0182--01", fromId: "iamsam_spain", amount: 20, toBankId: "at02-1465--01", toId: "iamben_ing")
         {
             let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
@@ -39,26 +35,8 @@ extension IntentHandler: INSendPaymentIntentHandling {
                     completion(INSendPaymentIntentResponse(code: .failure, userActivity: nil))
                     
                 } else {
-                    print ("success in Siri Intent makePayment");
+                    print ("Siri Intent sent \(amount) to \(payee)");
                     completion(INSendPaymentIntentResponse(code: .success, userActivity: nil))
-                }
-            }
-            task.resume()
-        }
-    }
-    
-    //https://my-reckoning.herokuapp.com/ReckonINGExample/createTransaction/iamsam?frombank_id=at02-0182--01&fromid=iamsam_spain&amount=20&tobank_id=at02-1465--01&toid=iamben_ing
-    func makePayment()
-    {
-        if let url = Server.createTransaction(me:"iamsam", fromBankId: "at02-0182--01", fromId: "iamsam_spain", amount: 20, toBankId: "at02-1465--01", toId: "iamben_ing")
-        {
-            let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-                if error != nil {
-                    // Display error message
-                    print ("error in Siri Intent makePayment");
-
-                } else {
-                    print ("success in Siri Intent makePayment");
                 }
             }
             task.resume()
@@ -66,7 +44,6 @@ extension IntentHandler: INSendPaymentIntentHandling {
     }
 }
 
-//class IntentHandler: INExtension, INSendMessageIntentHandling, INSearchForMessagesIntentHandling, INSetMessageAttributeIntentHandling
 class IntentHandler: INExtension
 {
     override func handler(for intent: INIntent) -> Any {
@@ -75,102 +52,5 @@ class IntentHandler: INExtension
         
         return self
     }
-    
-    /*
-    // MARK: - INSendMessageIntentHandling
-    
-    // Implement resolution methods to provide additional information about your intent (optional).
-    func resolveRecipients(forSendMessage intent: INSendMessageIntent, with completion: @escaping ([INPersonResolutionResult]) -> Void) {
-        if let recipients = intent.recipients {
-            
-            // If no recipients were provided we'll need to prompt for a value.
-            if recipients.count == 0 {
-                completion([INPersonResolutionResult.needsValue()])
-                return
-            }
-            
-            var resolutionResults = [INPersonResolutionResult]()
-            for recipient in recipients {
-                let matchingContacts = [recipient] // Implement your contact matching logic here to create an array of matching contacts
-                switch matchingContacts.count {
-                case 2  ... Int.max:
-                    // We need Siri's help to ask user to pick one from the matches.
-                    resolutionResults += [INPersonResolutionResult.disambiguation(with: matchingContacts)]
-                    
-                case 1:
-                    // We have exactly one matching contact
-                    resolutionResults += [INPersonResolutionResult.success(with: recipient)]
-                    
-                case 0:
-                    // We have no contacts matching the description provided
-                    resolutionResults += [INPersonResolutionResult.unsupported()]
-                    
-                default:
-                    break
-                    
-                }
-            }
-            completion(resolutionResults)
-        }
-    }
-    
-    func resolveContent(forSendMessage intent: INSendMessageIntent, with completion: @escaping (INStringResolutionResult) -> Void) {
-        if let text = intent.content, !text.isEmpty {
-            completion(INStringResolutionResult.success(with: text))
-        } else {
-            completion(INStringResolutionResult.needsValue())
-        }
-    }
-    
-    // Once resolution is completed, perform validation on the intent and provide confirmation (optional).
-    
-    func confirm(sendMessage intent: INSendMessageIntent, completion: @escaping (INSendMessageIntentResponse) -> Void) {
-        // Verify user is authenticated and your app is ready to send a message.
-        
-        let userActivity = NSUserActivity(activityType: NSStringFromClass(INSendMessageIntent.self))
-        let response = INSendMessageIntentResponse(code: .ready, userActivity: userActivity)
-        completion(response)
-    }
-    
-    // Handle the completed intent (required).
-    
-    func handle(sendMessage intent: INSendMessageIntent, completion: @escaping (INSendMessageIntentResponse) -> Void) {
-        // Implement your application logic to send a message here.
-        
-        let userActivity = NSUserActivity(activityType: NSStringFromClass(INSendMessageIntent.self))
-        let response = INSendMessageIntentResponse(code: .success, userActivity: userActivity)
-        completion(response)
-    }
-    
-    // Implement handlers for each intent you wish to handle.  As an example for messages, you may wish to also handle searchForMessages and setMessageAttributes.
-    
-    // MARK: - INSearchForMessagesIntentHandling
-    
-    func handle(searchForMessages intent: INSearchForMessagesIntent, completion: @escaping (INSearchForMessagesIntentResponse) -> Void) {
-        // Implement your application logic to find a message that matches the information in the intent.
-        
-        let userActivity = NSUserActivity(activityType: NSStringFromClass(INSearchForMessagesIntent.self))
-        let response = INSearchForMessagesIntentResponse(code: .success, userActivity: userActivity)
-        // Initialize with found message's attributes
-        response.messages = [INMessage(
-            identifier: "identifier",
-            content: "I am so excited about SiriKit!",
-            dateSent: Date(),
-            sender: INPerson(personHandle: INPersonHandle(value: "sarah@example.com", type: .emailAddress), nameComponents: nil, displayName: "Sarah", image: nil,  contactIdentifier: nil, customIdentifier: nil),
-            recipients: [INPerson(personHandle: INPersonHandle(value: "+1-415-555-5555", type: .phoneNumber), nameComponents: nil, displayName: "John", image: nil,  contactIdentifier: nil, customIdentifier: nil)]
-            )]
-        completion(response)
-    }
-    
-    // MARK: - INSetMessageAttributeIntentHandling
-    
-    func handle(setMessageAttribute intent: INSetMessageAttributeIntent, completion: @escaping (INSetMessageAttributeIntentResponse) -> Void) {
-        // Implement your application logic to set the message attribute here.
-        
-        let userActivity = NSUserActivity(activityType: NSStringFromClass(INSetMessageAttributeIntent.self))
-        let response = INSetMessageAttributeIntentResponse(code: .success, userActivity: userActivity)
-        completion(response)
-    }
-    */
 }
 
